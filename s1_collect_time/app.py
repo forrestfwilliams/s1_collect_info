05a0be570a82bc533e30790fe1d99d9f00b5ead7
Forrest Williams <ffwilliams2@alaska.edu>
2023-06-21 07:55:47 -0800

    split functionality

--- s1_collect_time/app.py
+++ s1_collect_time/app.py
@@ -1,99 +1,11 @@
 import json
-import requests
 import tempfile
 from argparse import ArgumentParser
-from datetime import datetime
-from lxml import etree
 from pathlib import Path
-from urllib.request import urlopen
 
 import asf_search as asf
-from bs4 import BeautifulSoup
 import geopandas as gpd
-from shapely import LinearRing
 from shapely.geometry import shape, Polygon
-import pandas as pd
-
-
-def scrape_esa_website_for_download_urls():
-    url = 'https://sentinel.esa.int/web/sentinel/missions/sentinel-1/observation-scenario/acquisition-segments'
-    page = urlopen(url)
-    html = page.read().decode('utf-8')
-    soup = BeautifulSoup(html, 'html.parser')
-    div = soup.find_all('div', class_='sentinel-1a')[0]
-    ul = div.find('ul')
-    hrefs = [a['href'] for a in ul.find_all('a')]
-    download_urls = [f'https://sentinel.esa.int{href}' for href in hrefs]
-    return download_urls[0:1]
-
-
-def download_kml(url, out_path='collection.kml'):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(out_path, 'wb') as file:
-            file.write(response.content)
-        print('File downloaded successfully.')
-    else:
-        print('Failed to download the file.')
-
-    return out_path
-
-
-def parse_placemark(placemark: etree.Element):
-    prefix = './/{http://www.opengis.net/kml/2.2}'
-
-    begin_date = placemark.find(f'{prefix}begin').text
-    begin_date = datetime.fromisoformat(begin_date)
-
-    end_date = placemark.find(f'{prefix}end').text
-    end_date = datetime.fromisoformat(end_date)
-
-    data = placemark.find(f'{prefix}ExtendedData')
-    mode = data.find(f"{prefix}Data[@name='Mode']").find(f'{prefix}value').text
-    orbit_absolute = int(data.find(f"{prefix}Data[@name='OrbitAbsolute']").find(f'{prefix}value').text)
-    orbit_relative = int(data.find(f"{prefix}Data[@name='OrbitRelative']").find(f'{prefix}value').text)
-
-    footprint = placemark.find(f'{prefix}LinearRing').find(f'{prefix}coordinates').text
-    x_coords = [float(point.split(',')[0]) for point in footprint.split(' ')]
-    y_coords = [float(point.split(',')[1]) for point in footprint.split(' ')]
-    footprint = LinearRing(zip(x_coords, y_coords))
-
-    return (begin_date, end_date, mode, orbit_absolute, orbit_relative, footprint)
-
-
-def parse_kml(kml_path: Path):
-    placemark_pattern = './/{http://www.opengis.net/kml/2.2}Placemark'
-    tree = etree.parse(kml_path).getroot()
-    placemarks = [parse_placemark(elem) for elem in tree.findall(placemark_pattern)]
-    columns = ['begin_date', 'end_date', 'mode', 'orbit_absolute', 'orbit_relative', 'geometry']
-    gdf = gpd.GeoDataFrame(data=placemarks, columns=columns, geometry='geometry', crs='EPSG:4326')
-    return gdf
-
-
-def prep_collection_plan(out_name='collection.geojson', dir=Path('.')):
-    urls = scrape_esa_website_for_download_urls()
-
-    gdfs = []
-    for url in urls:
-        name = Path(url).name
-        collection_gdf_path =dir / f'{name}.geojson'
-
-        if collection_gdf_path.exists():
-            print('Collection already prepared.')
-            gdf = gpd.read_file(collection_gdf_path)
-        else:
-            file_path = download_kml(url, dir / 'collection.kml')
-            gdf = parse_kml(file_path)
-            gdf.to_file(collection_gdf_path)
-        gdfs.append(gdf)
-
-    full_gdf = pd.concat(gdfs).drop_duplicates()
-    full_gdf = full_gdf.loc[full_gdf['begin_date'] >= datetime.now()].copy()
-    full_gdf = full_gdf.sort_values('begin_date', ascending=True).reset_index(drop=True)
-    
-    out_path = dir / out_name
-    full_gdf.to_file(out_path)
-    return out_path
 
 
 def get_granule_info(granule: str):
@@ -120,7 +32,6 @@ def find_valid_collect(gdf: gpd.GeoDataFrame, footprint: Polygon, mode: str, orb
 
 
 def get_next_collect(granule, dir=Path('.')):
-    prep_collection_plan('collection.geojson', dir)
     gdf = gpd.read_file(dir / 'collection.geojson')
     max_date = gdf['end_date'].max().date()
     footprint, mode, orbit_relative = get_granule_info(granule)
@@ -184,8 +95,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('granule')
     args = parser.parse_args()
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        message = get_next_collect(args.granule, dir = Path(tmpdirname))
+    message = get_next_collect(args.granule, dir = Path('.'))
     print(message)
 
 
