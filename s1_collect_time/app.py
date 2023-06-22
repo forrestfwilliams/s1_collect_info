9f013d6e446dc198417e291799b9939d3df3e51c
Forrest Williams <ffwilliams2@alaska.edu>
2023-06-21 17:00:09 -0800

    refactor insar interface

--- s1_collect_time/app.py
+++ s1_collect_time/app.py
@@ -1,16 +1,17 @@
 import json
 import os
-import tempfile
 from argparse import ArgumentParser
-from pathlib import Path
 
 import asf_search as asf
 import boto3
 import geopandas as gpd
-from shapely.geometry import shape, Polygon
+from shapely.geometry import shape, Polygon, Point
+from typing import Union
+
 
 S3 = boto3.client('s3')
 DATASET_BUCKET_NAME = os.environ.get('DatasetBucketName')
+COLLECTION_DATASET = gpd.read_file(f's3://{DATASET_BUCKET_NAME}/collection.geojson')
 
 
 def get_granule_info(granule: str):
@@ -22,28 +23,32 @@ def get_granule_info(granule: str):
     return footprint, mode, orbit_relative
 
 
-def find_valid_collect(gdf: gpd.GeoDataFrame, footprint: Polygon, mode: str, orbit_relative: int):
-    filtered = gdf.loc[(gdf['orbit_relative'] == orbit_relative) & (gdf['mode'] == mode)]
-    filtered = filtered.loc[filtered['geometry'].intersects(footprint)].copy()
+def find_valid_insar_collects(collections: gpd.GeoDataFrame, mode: str, orbit_relative: int):
+    filtered = collections.loc[(collections['orbit_relative'] == orbit_relative) & (collections['mode'] == mode)]
+    return filtered
 
-    if filtered.shape[0] > 0:
+
+def find_valid_collect(gdf: gpd.GeoDataFrame, footprint: Union[Polygon, Point]):
+    gdf = gdf.loc[gdf['geometry'].intersects(footprint)].copy()
+
+    if gdf.shape[0] > 0:
         collect_scheduled = True
-        filtered = filtered.sort_values('begin_date', ascending=True).reset_index(drop=True)
-        next_collect = filtered['begin_date'][0].date()
+        gdf = gdf.sort_values('begin_date', ascending=True).reset_index(drop=True)
+        next_collect = gdf['begin_date'][0].date()
     else:
         collect_scheduled = False
         next_collect = None
     return collect_scheduled, next_collect
 
 
-def get_next_collect(granule, dir=Path('.')):
-    gdf = gpd.read_file(dir / 'collection.geojson')
-    max_date = gdf['end_date'].max().date()
+def get_next_interferometric_collect(granule, collection_dataset):
     footprint, mode, orbit_relative = get_granule_info(granule)
-    collect_scheduled, next_collect = find_valid_collect(gdf, footprint, mode, orbit_relative)
+    valid_insar_collects = find_valid_insar_collects(collection_dataset, mode, orbit_relative)
+    collect_scheduled, next_collect = find_valid_collect(valid_insar_collects, footprint)
     if collect_scheduled:
         message = f'Next interferometrically valid collect is {next_collect}'
     else:
+        max_date = collection_dataset['end_date'].max().date()
         message = f'No interferometrically valid collect is scheduled on or before {max_date}'
 
     return message
@@ -51,12 +56,7 @@ def get_next_collect(granule, dir=Path('.')):
 
 def lambda_handler(event, context):
     granule = event['pathParameters']['granule']
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        tmpdir = Path(tmpdirname)
-        collection_name = 'collection.geojson'
-        tmp_collection = str(tmpdir / collection_name)
-        S3.download_file(DATASET_BUCKET_NAME, collection_name, tmp_collection)
-        message = get_next_collect(granule, dir=tmpdir)
+    message = get_next_interferometric_collect(granule, COLLECTION_DATASET)
 
     return {
         'statusCode': 200,
@@ -74,7 +74,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('granule')
     args = parser.parse_args()
-    message = get_next_collect(args.granule, dir=Path('.'))
+    message = get_next_interferometric_collect(args.granule, COLLECTION_DATASET)
     print(message)
 
 
